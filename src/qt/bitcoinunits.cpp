01db600dda4fa918e87e15675b0a5ae8bfd8a811
barcoin <barcoin@mailinator.com>
2013-05-26 22:59:13 -0600

    changes

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -34,9 +34,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("GEO");
-    case mBTC: return QString("mGEO");
-    case uBTC: return QString::fromUtf8("μGEO");
+    case BTC: return QString("FOO");
+    case mBTC: return QString("mFOO");
+    case uBTC: return QString::fromUtf8("μFOO");
     default: return QString("???");
     }
 }
